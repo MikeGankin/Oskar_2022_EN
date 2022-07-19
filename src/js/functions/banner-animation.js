2c5349f07c2b4512bf5db26de2688dc844ae29a1
Михаил <mikhail-gankin@yandex.ru>
2022-07-19 23:47:15 +0300

    Add sections

---
+++ src/js/functions/banner-animation.js
@@ -0,0 +1,7 @@
+export const bannerAnimation = () => {
+    let oskar = document.querySelector('.oskar');
+    let hartmann = document.querySelector('.hartmann');
+    let unicorn = document.querySelector('.unicorn');
+    oskar.style.animationName = 'slideInBanner';
+}
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import './vendor/locomotive-scroll.js';
 import {menuAnimation} from "./functions/menu-animation";
 import {player} from "./functions/player";
+import {bannerAnimation} from "./functions/banner-animation";
 
 const burger = document.querySelector('.menu__btn');
 
 burger.addEventListener('click', menuAnimation);
-window.addEventListener('load', player());
+window.addEventListener('load', player(), bannerAnimation);
 
 
 
