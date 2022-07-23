b377ce72c7dbac72592ff76137555bf1db50c2d3
Михаил <mikhail-gankin@yandex.ru>
2022-07-23 15:10:18 +0300

    scroll-fix

--- src/js/vendor/locomotive-scroll.js
+++ src/js/vendor/locomotive-scroll.js
@@ -2,10 +2,12 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
+
     tablet: {
-        smooth: true
+      smooth: true,
     },
+
     smartphone: {
-        smooth: true
-    },
+      smooth: true,
+    }
 });
